1f31e70e44730e7f95e855b0fcffdcd0daa30795
Aditya Kumar Singh <56215647+adi2710@users.noreply.github.com>
2022-10-18 13:02:44 +0530

    Time: 87 ms (96.96%), Space: 38.3 MB (49.73%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
@@ -2,25 +2,13 @@ class Solution {
 public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
         int n = arr.size();
-        vector<vector<int>> pre(n + 1, vector<int> (31));
+        vector<int> pre(n + 1);
         for(int i = 1; i <= n; i++)
-        {
-            for(int bit = 0; bit <= 30; bit++)
-            {
-                int mask = 1 << bit;
-                pre[i][bit] = pre[i - 1][bit] + ((arr[i - 1] & mask) == 0 ? 0 : 1);
-            }
-        }
+            pre[i] = pre[i - 1] ^ arr[i - 1];
         vector<int> ans;
         for(auto &query : queries)
         {
-            int l = query[0], r = query[1] + 1, xorSum = 0;
-            for(int bit = 0; bit <= 30; bit++)
-            {
-                int cnt = pre[r][bit] - pre[l][bit];
-                if(cnt & 1)
-                    xorSum |= (1 << bit);
-            }
+            int l = query[0], r = query[1] + 1, xorSum = pre[r] ^ pre[l];
             ans.push_back(xorSum);
         }
         return ans;
