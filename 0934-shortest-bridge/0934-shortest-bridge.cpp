520f78d7bf6db7495a27742085ffdc713d76fd86
Aditya Kumar Singh <56215647+adi2710@users.noreply.github.com>
2022-10-27 13:24:23 +0530

    Time: 135 ms (33.18%), Space: 19.1 MB (78.60%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.cpp
+++ 0934-shortest-bridge/0934-shortest-bridge.cpp
@@ -3,34 +3,37 @@ public:
     
     vector<int> dx = {-1, 1, 0, 0}, dy = {0, 0, -1, 1};
     
-    void dfs(int r, int c, vector<vector<int>> &grid, set<pair<int, int>> &vis)
+    void dfs(int r, int c, vector<vector<int>> &grid, queue<pair<int, int>> &q, vector<vector<bool>> &vis)
     {
-        vis.insert({r, c});
+        q.push({r, c});
         grid[r][c] = 0;
+        vis[r][c] = true;
         for(int idx = 0; idx < 4; idx++)
         {
             int row = r + dx[idx], col = c + dy[idx];
-            if(0 <= row && row < grid.size() && 0 <= col && col < grid[0].size() && grid[row][col] == 1 && vis.find({row, col}) == vis.end())
-                dfs(row, col, grid, vis);
+            if(0 <= row && row < grid.size() && 0 <= col && col < grid[0].size() && grid[row][col] == 1)
+                dfs(row, col, grid, q, vis);
         }
     }
     
     int shortestBridge(vector<vector<int>>& grid) {
-        set<pair<int, int>> island1, island2;
         int n = grid.size(), m = grid[0].size();
+        vector<vector<bool>> vis(n, vector<bool> (m));
+        queue<pair<int, int>> q;
         for(int i = 0; i < n; i++)
         {
+            bool fl = false;
             for(int j = 0; j < m; j++)
             {
-                if(grid[i][j] == 1 && island1.empty())
-                    dfs(i, j, grid, island1);
-                else if(grid[i][j] == 1 && !island1.empty())
-                    dfs(i, j, grid, island2);
+                if(grid[i][j] == 1){
+                    dfs(i, j, grid, q, vis);
+                    fl = true;
+                    break;
+                }
             }
+            if(fl)
+                break;
         }
-        queue<pair<int, int>> q;
-        for(auto coor : island1)
-            q.push(coor);
         int ans = 0;
         while(!q.empty())
         {
@@ -38,15 +41,15 @@ public:
             {
                 auto [r, c] = q.front();
                 q.pop();
-                if(island2.find({r, c}) != island2.end())
+                if(grid[r][c] == 1)
                     return ans - 1;
                 for(int idx = 0; idx < 4; idx++)
                 {
                     int row = r + dx[idx], col = c + dy[idx];
-                    if(0 <= row && row < n && 0 <= col && col < m && island1.find({row, col}) == island1.end())
+                    if(0 <= row && row < n && 0 <= col && col < m && !vis[row][col])
                     {
                         q.push({row, col});
-                        island1.insert({row, col});
+                        vis[row][col] = true;
                     }
                 }
             }
