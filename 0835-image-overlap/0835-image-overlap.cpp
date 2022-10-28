a6c33f2501998b09f747f394c6bb63ddf8784888
Aditya Kumar Singh <56215647+adi2710@users.noreply.github.com>
2022-10-28 10:36:29 +0530

    Time: 1072 ms (5.56%), Space: 12.7 MB (9.03%) - LeetHub

---
+++ 0835-image-overlap/0835-image-overlap.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    
+    struct compare{
+        size_t operator()(const pair<int, int> &a) const
+        {
+            return a.first ^ a.second;
+        }
+    };
+    
+    vector<pair<int, int>> nonZeroCells(vector<vector<int>> &img)
+    {
+        vector<pair<int, int>> vec;
+        for(int i = 0; i < img.size(); i++)
+        {
+            for(int j = 0; j < img[0].size(); j++)
+            {
+                if(img[i][j] == 1)
+                    vec.push_back({i, j});
+            }
+        }
+        return vec;
+    }
+    
+    int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
+        vector<pair<int, int>> ar = nonZeroCells(img1);
+        vector<pair<int, int>> br = nonZeroCells(img2);
+        int ans = 0;
+        unordered_map<pair<int, int>, int, compare> groupCnt;
+        for(auto a : ar)
+        {
+            for(auto b : br)
+            {
+                pair<int, int> dis = {a.first - b.first, a.second - b.second};
+                groupCnt[dis]++;
+                ans = max(ans, groupCnt[dis]);
+            }
+        }
+        return ans;
+    }
+};
