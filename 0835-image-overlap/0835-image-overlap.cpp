d4d144a2ac172ca0c7575b2d55a76cf1f36d0ded
Aditya Kumar Singh <56215647+adi2710@users.noreply.github.com>
2022-10-28 10:41:25 +0530

    Time: 162 ms (52.78%), Space: 12.1 MB (43.06%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.cpp
+++ 0835-image-overlap/0835-image-overlap.cpp
@@ -1,42 +1,29 @@
 class Solution {
 public:
-    
-    struct compare{
-        size_t operator()(const pair<int, int> &a) const
-        {
-            auto hash1 = hash<int>{}(a.first);
-            auto hash2 = hash<int>{}(a.second);
-            
-            if(hash1 != hash2)
-                return hash1 ^ hash2;
-            return hash1;
-        }
-    };
-    
-    vector<pair<int, int>> nonZeroCells(vector<vector<int>> &img)
+    vector<int> nonZeroCells(vector<vector<int>> &img)
     {
-        vector<pair<int, int>> vec;
+        vector<int> vec;
         for(int i = 0; i < img.size(); i++)
         {
             for(int j = 0; j < img[0].size(); j++)
             {
                 if(img[i][j] == 1)
-                    vec.push_back({i, j});
+                    vec.push_back(i * 100 + j);
             }
         }
         return vec;
     }
     
     int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
-        vector<pair<int, int>> ar = nonZeroCells(img1);
-        vector<pair<int, int>> br = nonZeroCells(img2);
+        vector<int> ar = nonZeroCells(img1);
+        vector<int> br = nonZeroCells(img2);
         int ans = 0;
-        unordered_map<pair<int, int>, int, compare> groupCnt;
-        for(auto a : ar)
+        unordered_map<int, int> groupCnt;
+        for(int a : ar)
         {
-            for(auto b : br)
+            for(int b : br)
             {
-                pair<int, int> dis = {a.first - b.first, a.second - b.second};
+                int dis = a - b;
                 groupCnt[dis]++;
                 ans = max(ans, groupCnt[dis]);
             }
