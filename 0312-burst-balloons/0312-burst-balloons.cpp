af12efd14e864ddc312d7072e4baca6e87b62145
Aditya Kumar Singh <56215647+adi2710@users.noreply.github.com>
2022-11-11 15:54:37 +0530

    Time: 586 ms (75.52%), Space: 10.3 MB (54.07%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,28 +1,18 @@
 class Solution {
 public:
-    
-    int dp[301][301];
-    
-    int solve(int l, int r, vector<int> &nums)
-    {
-        if(l > r)
-            return 0;
-        
-        if(dp[l][r] != -1)
-            return dp[l][r];
-        
-        int res = 0;
-        for(int mid = l; mid <= r; mid++)
-        {
-            res = max(res, nums[l - 1] * nums[mid] * nums[r + 1] + solve(l, mid - 1, nums) + solve(mid + 1, r, nums));
-        }
-        return dp[l][r] = res;
-    }
-    
     int maxCoins(vector<int>& nums) {
-        memset(dp, -1, sizeof(dp));
         nums.insert(nums.begin(), 1);
         nums.push_back(1);
-        return solve(1, nums.size() - 2, nums);
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int> (n));
+        for(int gap = 0; gap < n; gap++)
+        {
+            for(int l = 1, r = 1 + gap; l + 1 < n && r + 1 < n; l++, r++)
+            {
+                for(int mid = l; mid <= r; mid++)
+                    dp[l][r] = max(dp[l][r], nums[l - 1] * nums[mid] * nums[r + 1] + dp[l][mid - 1] + dp[mid + 1][r]);
+            }
+        }
+        return dp[1][n - 2];
     }
 };
