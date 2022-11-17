06f53fbed5107f2d83386c421e8933688df12433
Aditya Kumar Singh <56215647+adi2710@users.noreply.github.com>
2022-11-17 11:19:26 +0530

    Time: 16 ms (51.22%), Space: 6 MB (32.00%) - LeetHub

--- 0223-rectangle-area/0223-rectangle-area.cpp
+++ 0223-rectangle-area/0223-rectangle-area.cpp
@@ -8,8 +8,10 @@ public:
     
     int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
         int area = calculateArea(ax1, ay1, ax2, ay2) + calculateArea(bx1, by1, bx2, by2);
-        if(((ax1 <= bx1 && bx1 <= ax2) || (ax1 <= bx2 && bx2 <= ax2) || (bx1 <= ax1 && ax1 <= bx2) || (bx1 <= ax2 && ax2 <= bx2)) && ((ay1 <= by1 && by1 <= ay2) || (ay1 <= by2 && by2 <= ay2) || (by1 <= ay1 && ay1 <= by2) || (by1 <= ay2 && ay2 <= by2)))
-            area -= calculateArea(max(ax1, bx1), max(ay1, by1), min(ax2, bx2), min(ay2, by2));
+        int left = max(ax1, bx1), right = min(ax2, bx2);
+        int bottom = max(ay1, by1), top = min(ay2, by2);
+        if(left <= right && bottom <= top)
+            area -= calculateArea(left, bottom, right, top);
         return area;
     }
 };
