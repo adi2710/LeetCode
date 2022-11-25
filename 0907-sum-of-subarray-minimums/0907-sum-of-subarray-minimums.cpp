17b4724f1ec6a086b75f383df8b7f0789fb0f3b3
Aditya Kumar Singh <56215647+adi2710@users.noreply.github.com>
2022-11-25 10:39:43 +0530

    Time: 102 ms (94.68%), Space: 39.4 MB (96.65%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int sumSubarrayMins(vector<int>& arr) {
-        arr.push_back(0);
-        stack<int> st;
-        st.push(-1);
         int ans = 0;
         const int MOD = 1e9 + 7;
+        stack<int> st;
+        st.push(-1);
+        arr.push_back(0);
         for(int i = 0; i < arr.size(); i++)
         {
             while(st.size() > 1 && arr[st.top()] >= arr[i])
             {
-                int idx = st.top();
+                int pos = st.top();
                 st.pop();
-                int lf = idx - st.top(), rt = i - idx;
-                ans = (ans + 1ll * arr[idx] * lf * rt) % MOD;
+                int sum = (pos - st.top()) * (i - pos);
+                ans = (1ll * ans + 1ll * sum * arr[pos]) % MOD;
             }
             st.push(i);
         }
