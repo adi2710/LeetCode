679c1322c3cd75ea84335ce936f0924987a67f81
Aditya Kumar Singh <56215647+adi2710@users.noreply.github.com>
2022-12-03 12:29:28 +0530

    Time: 2194 ms (5.04%), Space: 80.2 MB (17.68%) - LeetHub

--- 0313-super-ugly-number/0313-super-ugly-number.cpp
+++ 0313-super-ugly-number/0313-super-ugly-number.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     int nthSuperUglyNumber(int n, vector<int>& primes) {
-        vector<long> dp(n), pointers(primes.size());
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        vector<int> dp(n);
         dp[0] = 1;
+        for(int i = 0; i < primes.size(); i++)
+            pq.push({primes[i], i, 0});
         for(int i = 1; i < n; i++)
         {
-            long mn = INT_MAX;
-            for(int j = 0; j < primes.size(); j++)
-                mn = min(mn, dp[pointers[j]] * primes[j]);
+            int mn = pq.top()[0];
             dp[i] = mn;
-            for(int j = 0; j < primes.size(); j++)
+            while(!pq.empty() && pq.top()[0] == mn)
             {
-                if(mn == dp[pointers[j]] * primes[j])
-                    pointers[j]++;
+                vector<int> a = pq.top();
+                pq.pop();
+                if(1ll * dp[a[2] + 1] * primes[a[1]] <= INT_MAX)
+                    pq.push({dp[a[2] + 1] * primes[a[1]], a[1], a[2] + 1});
             }
         }
         return dp[n - 1];
