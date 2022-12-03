e71c481a8851231417bf52b19c52663a9e21160a
Aditya Kumar Singh <56215647+adi2710@users.noreply.github.com>
2022-12-03 12:34:11 +0530

    Time: 207 ms (94.32%), Space: 13.2 MB (23.61%) - LeetHub

--- 0313-super-ugly-number/0313-super-ugly-number.cpp
+++ 0313-super-ugly-number/0313-super-ugly-number.cpp
@@ -1,23 +1,27 @@
+#define P pair<long long, int>
 class Solution {
 public:
     int nthSuperUglyNumber(int n, vector<int>& primes) {
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
-        vector<int> dp(n);
-        dp[0] = 1;
-        for(int i = 0; i < primes.size(); i++)
-            pq.push({primes[i], i, 0});
-        for(int i = 1; i < n; i++)
-        {
-            int mn = pq.top()[0];
-            dp[i] = mn;
-            while(!pq.empty() && pq.top()[0] == mn)
+        vector<int> index(primes.size(), 0);
+            vector<long long> nums;
+            nums.push_back(1);      
+            
+            priority_queue<P, vector<P>, greater<P>> pq;    // remember pair(the ugly number, who generated this ugly)
+            for (int i = 0; i < primes.size(); i++)
+                pq.emplace(primes[i], i);   // initial ugly = prime  * 1
+            
+            while (nums.size() < n)
             {
-                vector<int> a = pq.top();
+                auto [value, id] = pq.top();    // get the smallest ugly
                 pq.pop();
-                if(1ll * dp[a[2] + 1] * primes[a[1]] <= INT_MAX)
-                    pq.push({dp[a[2] + 1] * primes[a[1]], a[1], a[2] + 1});
+                
+                if (value != nums.back())       // check the dupe
+                    nums.push_back(value);
+                
+                index[id]++;                             // the prime generated this small ugly number should move forward to multiply next ugly number
+                pq.emplace(nums[index[id]] * primes[id], id);         // push the new ugly number to pq to participate sorting
             }
+            
+            return nums.back();
         }
-        return dp[n - 1];
-    }
 };
