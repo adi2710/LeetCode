584d1efb29f6425e682a6ea1ca4f45e7fd442826
Aditya Kumar Singh <56215647+adi2710@users.noreply.github.com>
2022-12-12 09:05:37 +0530

    Time: 3 ms (34.80%), Space: 5.9 MB (55.72%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -3,10 +3,10 @@ public:
     int climbStairs(int n) {
         if(n == 1)
             return n;
-        int a = 1, b = 1;
+        int a = 1, b = 1, c;
         for(int i = 2; i <= n; i++)
         {
-            int c = a + b;
+            c = a + b;
             a = b;
             b = c;
         }
