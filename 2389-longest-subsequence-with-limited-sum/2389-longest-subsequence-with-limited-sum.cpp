76e957b8466c42e9948d000c2a7d22dac716ed1d
Aditya Kumar Singh <56215647+adi2710@users.noreply.github.com>
2022-12-25 10:42:07 +0530

    Time: 21 ms (88.30%), Space: 13.4 MB (98.20%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
@@ -2,19 +2,13 @@ class Solution {
 public:
     vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
         sort(nums.begin(), nums.end());
-        for(int &query : queries)
-        {
-            int sum = 0, count = 0;
-            for(int num : nums)
-            {
-                if(sum + num <= query)
-                {
-                    sum += num;
-                    count++;
-                }
-            }
-            query = count;
+        int pre = 0;
+        for(int &num : nums){
+            pre += num;
+            num = pre;
         }
+        for(int &query : queries)
+            query = upper_bound(nums.begin(), nums.end(), query) - nums.begin();
         return queries;
     }
 };
