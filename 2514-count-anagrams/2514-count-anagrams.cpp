c89e6c253b593a0d2d3743b47c2bcec165b24355
Aditya Kumar Singh <56215647+adi2710@users.noreply.github.com>
2022-12-27 12:13:43 +0530

    Time: 230 ms (25.90%), Space: 23 MB (53.87%) - LeetHub

---
+++ 2514-count-anagrams/2514-count-anagrams.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    
+    static const int N = 1e5 + 5, MOD = 1e9 + 7;
+    int fact[N];
+    
+    void precompute()
+    {
+        fact[0] = 1;
+        for(int i = 1; i < N; i++)
+            fact[i] = (1ll * fact[i - 1] * i) % MOD;
+    }
+    
+    int power(int a, int b)
+    {
+        int res = 1;
+        while(b)
+        {
+            if(b & 1)
+                res = (1ll * res * a) % MOD;
+            a = (1ll * a * a) % MOD;
+            b >>= 1;
+        }
+        return res;
+    }
+    
+    int countAnagrams(string s) {
+        precompute();
+        stringstream ss(s);
+        string word;
+        int ans = 1;
+        while(ss >> word)
+        {
+            vector<int> freq(26);
+            ans = (1ll * ans * fact[word.size()]) % MOD;
+            for(char ch : word)
+                freq[ch - 'a']++;
+            for(int i = 0; i < 26; i++)
+                ans = (1ll * ans * power(fact[freq[i]], MOD - 2)) % MOD;
+        }
+        return ans;
+    }
+};
