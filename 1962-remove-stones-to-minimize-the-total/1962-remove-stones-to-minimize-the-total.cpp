fc33a1536eb4934bc272751c3d90bfd2ac0afd87
Aditya Kumar Singh <56215647+adi2710@users.noreply.github.com>
2022-12-28 11:42:39 +0530

    Time: 551 ms (92.07%), Space: 98.9 MB (70.86%) - LeetHub

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.cpp
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     int minStoneSum(vector<int>& piles, int k) {
-        priority_queue<int> pq;
-        for(int stones : piles)
-            pq.push(stones);
+        priority_queue<int> pq(piles.begin(), piles.end());
+        int ans = accumulate(piles.begin(), piles.end(), 0);
         while(k--)
         {
             int stones = pq.top();
             pq.pop();
+            ans -= stones / 2;
             pq.push(stones - stones / 2);
-        }
-        int ans = 0;
-        while(!pq.empty())
-        {
-            ans += pq.top();
-            pq.pop();
         }
         return ans;
     }
