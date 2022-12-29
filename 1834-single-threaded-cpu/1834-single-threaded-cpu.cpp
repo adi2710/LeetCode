9f4d82af094d84d615257bcc5fdbc6fc953aacfa
Aditya Kumar Singh <56215647+adi2710@users.noreply.github.com>
2022-12-29 12:49:51 +0530

    Time: 580 ms (94.71%), Space: 129.4 MB (68.25%) - LeetHub

---
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> getOrder(vector<vector<int>>& tasks) {
+        for(int i = 0; i < tasks.size(); i++)
+            tasks[i].push_back(i);
+        sort(tasks.begin(), tasks.end(), [](const vector<int> &a, const vector<int> &b){
+            return a[0] < b[0];
+        });
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        int i = 0;
+        long curTime = 1;
+        vector<int> ans;
+        while(i < tasks.size() || !pq.empty())
+        {
+            while(i < tasks.size() && curTime >= tasks[i][0])
+                pq.push({tasks[i][1], tasks[i][2]}), i++;
+            if(pq.empty())
+            {
+                curTime = i < tasks.size() ? tasks[i][0] : curTime;
+                continue;
+            }
+            ans.push_back(pq.top().second);
+            curTime += pq.top().first;
+            pq.pop();
+        }
+        return ans;
+    }
+};
