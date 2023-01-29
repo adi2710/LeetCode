1143e6c73862cff252937b063118ddd8a9cdb508
Aditya Kumar Singh <56215647+adi2710@users.noreply.github.com>
2023-01-29 22:20:59 +0530

    Time: 503 ms (91.18%), Space: 179.9 MB (59.50%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.cpp
+++ 0460-lfu-cache/0460-lfu-cache.cpp
@@ -1,8 +1,7 @@
 class LFUCache {
 public:
     unordered_map<int, list<pair<int, int>>> lfu_cache;
-    unordered_map<int, list<pair<int, int>>::iterator> key_map;
-    unordered_map<int, int> key_freq;
+    unordered_map<int, pair<int, list<pair<int, int>>::iterator>> key_map;
     int capacity, cur_size, min_freq;
     LFUCache(int capacity) {
         this -> capacity = capacity;
@@ -13,8 +12,8 @@ public:
     int get(int key) {
         if(key_map.find(key) == key_map.end())
             return -1;
-        auto it = key_map[key];
-        int freq = key_freq[key], value = (*it).second;
+        auto it = key_map[key].second;
+        int freq = key_map[key].first, value = (*it).second;
         if(lfu_cache[freq].size() == 1)
         {
             if(min_freq == freq)
@@ -24,36 +23,33 @@ public:
         else
             lfu_cache[freq].erase(it);
         lfu_cache[freq + 1].push_back({key, value});
-        key_map[key] = --(lfu_cache[freq + 1].end());
-        key_freq[key]++;
+        key_map[key] = {freq + 1, --(lfu_cache[freq + 1].end())};
         return value;
     }
     
     void put(int key, int value) {
         if(capacity == 0)
             return;
-        if(key_freq.find(key) == key_freq.end())
+        if(key_map.find(key) == key_map.end())
         {
             if(cur_size == capacity)
             {
                 auto it = lfu_cache[min_freq].begin();
                 int lfu_key = (*it).first;
                 lfu_cache[min_freq].erase(it);
-                key_freq.erase(lfu_key);
                 key_map.erase(lfu_key);
                 if(lfu_cache[min_freq].empty())
                     lfu_cache.erase(min_freq);
                 cur_size--;
             }
             lfu_cache[1].push_back({key, value});
-            key_freq[key] = 1;
-            key_map[key] = --(lfu_cache[1].end());
+            key_map[key] = {1, --(lfu_cache[1].end())};
             min_freq = 1;
             cur_size++;
         }
         else
         {
-            auto it = key_map[key];
+            auto it = key_map[key].second;
             (*it).second = value;
             get(key);
         }
