7f264891ca06c9e9c3291e399d9d857596fcfefc
Aditya Kumar Singh <56215647+adi2710@users.noreply.github.com>
2023-01-29 22:17:19 +0530

    Time: 560 ms (64.03%), Space: 184.9 MB (46.04%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.cpp
+++ 0460-lfu-cache/0460-lfu-cache.cpp
@@ -1,51 +1,61 @@
 class LFUCache {
 public:
-    unordered_map<int, list<pair<int, int>>> ls;
-    unordered_map<int, pair<int, list<pair<int, int>> :: iterator>> freq;
-    int sz, minFreq;
+    unordered_map<int, list<pair<int, int>>> lfu_cache;
+    unordered_map<int, list<pair<int, int>>::iterator> key_map;
+    unordered_map<int, int> key_freq;
+    int capacity, cur_size, min_freq;
     LFUCache(int capacity) {
-        ls.clear();
-        freq.clear();
-        sz = capacity;
-        minFreq = 0;
+        this -> capacity = capacity;
+        cur_size = 0;
+        min_freq = 0;
     }
     
     int get(int key) {
-        if(freq.find(key) == freq.end())
+        if(key_map.find(key) == key_map.end())
             return -1;
-        auto it = freq[key].second;
-        int val = (*it).second;
-        put(key, val);
-        return val;
+        auto it = key_map[key];
+        int freq = key_freq[key], value = (*it).second;
+        if(lfu_cache[freq].size() == 1)
+        {
+            if(min_freq == freq)
+                min_freq++;
+            lfu_cache.erase(freq);
+        }
+        else
+            lfu_cache[freq].erase(it);
+        lfu_cache[freq + 1].push_back({key, value});
+        key_map[key] = --(lfu_cache[freq + 1].end());
+        key_freq[key]++;
+        return value;
     }
     
     void put(int key, int value) {
-        if(freq.find(key) != freq.end())
-        {
-            auto it = freq[key].second;
-            int fr = freq[key].first;
-            ls[fr].erase(it);
-            if(ls[fr].empty()){
-                ls.erase(fr);
-                if(minFreq == fr)
-                    minFreq++;
-            }
-            ls[fr + 1].push_front({key, value});
-            freq[key] = {fr + 1, ls[fr + 1].begin()};
+        if(capacity == 0)
             return;
-        }
-        if(freq.size() == sz && minFreq >= 1)
+        if(key_freq.find(key) == key_freq.end())
         {
-            int key = ls[minFreq].back().first;
-            freq.erase(key);
-            ls[minFreq].erase(--ls[minFreq].end());
-            if(ls[minFreq].empty())
-                ls.erase(minFreq);
+            if(cur_size == capacity)
+            {
+                auto it = lfu_cache[min_freq].begin();
+                int lfu_key = (*it).first;
+                lfu_cache[min_freq].erase(it);
+                key_freq.erase(lfu_key);
+                key_map.erase(lfu_key);
+                if(lfu_cache[min_freq].empty())
+                    lfu_cache.erase(min_freq);
+                cur_size--;
+            }
+            lfu_cache[1].push_back({key, value});
+            key_freq[key] = 1;
+            key_map[key] = --(lfu_cache[1].end());
+            min_freq = 1;
+            cur_size++;
         }
-        if(freq.size() < sz){
-            ls[1].push_front({key, value});
-            freq[key] = {1, ls[1].begin()};
-            minFreq = 1;
+        else
+        {
+            auto it = key_map[key];
+            (*it).second = value;
+            get(key);
         }
     }
 };
@@ -56,3 +66,5 @@ public:
  * int param_1 = obj->get(key);
  * obj->put(key,value);
  */
+// 1- 3
+// 2- 1
