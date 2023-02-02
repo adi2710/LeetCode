477d30ee13738223c3fad3592e93a10f6d4ce455
Aditya Kumar Singh <56215647+adi2710@users.noreply.github.com>
2023-02-02 17:03:10 +0530

    Time: 691 ms (32.49%), Space: 0B (100.00%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,1 +1,3 @@
-SELECT Email FROM (SELECT Email, COUNT(Email) AS cnt FROM Person GROUP BY Email) AS sub WHERE sub.cnt > 1;
+# SELECT Email FROM (SELECT Email, COUNT(Email) AS cnt FROM Person GROUP BY Email) AS sub WHERE sub.cnt > 1;
+
+SELECT Email FROM Person GROUP BY Email HAVING COUNT(Email) > 1;
