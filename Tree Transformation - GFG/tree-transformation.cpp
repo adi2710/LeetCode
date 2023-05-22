a8de4ec4a3874a1f09d36e12da432af882c5707f
Aditya Kumar Singh <56215647+adi2710@users.noreply.github.com>
2023-05-22 10:32:46 +0530

    Added solution - LeetHub

---
+++ Tree Transformation - GFG/tree-transformation.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+    public:
+    int solve(int N, vector<int> p){
+        // code here
+        unordered_set<int> st;
+        for(int x : p)
+        {
+            if(x > 0)
+                st.insert(x);
+        }
+        return st.size();
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int T;
+    cin >> T;
+    while(T--){
+        int N;
+        cin >> N;
+        vector<int> p(N);
+        for(int i = 0; i < N; i++){
+            cin >> p[i];
+        }
+
+        Solution obj;
+        cout << obj.solve(N, p) << "\n";
+    }
+}   
+// } Driver Code Ends
