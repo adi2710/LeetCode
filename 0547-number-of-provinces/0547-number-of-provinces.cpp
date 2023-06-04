6f4ec30ba5c5a75bcc6616eab790ab9a61040425
Aditya Kumar Singh <56215647+adi2710@users.noreply.github.com>
2023-06-04 12:02:23 +0530

    Time: 30 ms (36.74%), Space: 13.7 MB (87.38%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,37 +1,43 @@
 class Solution {
 public:
     
-    void bfs(int cur, vector<bool> &vis, vector<vector<int>> &isConnected)
+    int find(int cur, vector<int> &par)
     {
-        vis[cur] = 1;
-        queue<int> q;
-        q.push(cur);
-        while(!q.empty())
+        if(par[cur] == cur)
+            return cur;
+        return par[cur] = find(par[cur], par);
+    }
+    
+    void unite(int u, int v, vector<int> &rank, vector<int> &par, int &comp)
+    {
+        int par_u = find(u, par), par_v = find(v, par);
+        if(par_u == par_v)
+            return;
+        comp--;
+        if(rank[par_u] == rank[par_v])
         {
-            cur = q.front();
-            q.pop();
-            for(int i = 0; i < isConnected.size(); i++)
-            {
-                if(isConnected[cur][i] && !vis[i])
-                {
-                    vis[i] = 1;
-                    q.push(i);
-                }
-            }
+            par[par_v] = par_u;
+            rank[par_u]++;
         }
+        else if(rank[par_u] > rank[par_v])
+            par[par_v] = par_u;
+        else
+            par[par_u] = par_v;
     }
     
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size(), ans = 0;
-        vector<bool> vis(n);
+        int n = isConnected.size();
+        int comp = n;
+        vector<int> par(n), rank(n, 1);
+        iota(par.begin(), par.end(), 0);
         for(int i = 0; i < n; i++)
         {
-            if(!vis[i])
+            for(int j = 0; j < n; j++)
             {
-                bfs(i, vis, isConnected);
-                ans++;
+                if(isConnected[i][j])
+                    unite(i, j, rank, par, comp);
             }
         }
-        return ans;
+        return comp;
     }
 };
