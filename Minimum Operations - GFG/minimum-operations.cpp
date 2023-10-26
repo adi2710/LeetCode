e76a252afd9ccbd9ad36f736e4c2cfe38aa54233
Aditya Kumar Singh <56215647+adi2710@users.noreply.github.com>
2023-10-26 20:52:00 +0530

    Added solution - LeetHub

---
+++ Minimum Operations - GFG/minimum-operations.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    int minOperation(int n)
+    {
+        //code here.
+        int msb = __builtin_clz(n), ans = 0;
+        while(n)
+        {
+            ans++;
+            n &= (n - 1);
+        }
+        return ans + (31 - msb);
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	    {
+	        int n;
+	        cin>>n;
+	        Solution ob;
+	        cout<<ob.minOperation(n)<<endl;
+	    }
+}
+// } Driver Code Ends
