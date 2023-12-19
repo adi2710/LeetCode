ef60f1fc18a8308fe95380f9488266fecf2ff271
Aditya Kumar Singh <56215647+adi2710@users.noreply.github.com>
2023-12-19 21:06:33 +0530

    Time: 48 ms (30.15%), Space: 22.1 MB (59.38%) - LeetHub

--- 0661-image-smoother/0661-image-smoother.cpp
+++ 0661-image-smoother/0661-image-smoother.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
         int n = img.size(), m = img[0].size();
-        vector<vector<int>> ans(n, vector<int> (m));
+        const int MOD = 256;
         vector<int> dx = {-1, 0, 1};
         for(int r = 0; r < n; r++) {
             for(int c = 0; c < m; c++) {
@@ -12,13 +12,18 @@ public:
                         int row = r + dr, col = c + dc;
                         if(row < 0 || row >= n || col < 0 || col >= m)
                             continue;
-                        sum += img[row][col];
+                        sum += img[row][col] % MOD;
                         count++;
                     }
                 }
-                ans[r][c] = sum / count;
+                img[r][c] = sum / count * MOD + img[r][c];
             }
         }
-        return ans;
+        for(int r = 0; r < n; r++) {
+            for(int c = 0; c < m; c++) {
+                img[r][c] /= MOD;
+            }
+        }
+        return img;
     }
 };
