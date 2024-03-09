e7495511252d2bb6987ff635d405d3e92c763dc3
Aditya Kumar Singh <56215647+adi2710@users.noreply.github.com>
2024-03-09 20:55:51 +0530

    Time: 5 ms (45.65%), Space: 7.8 MB (83.58%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
-    
-    bool match(string &text, string &pattern)
-    {
-        for(int i = 0; i < text.size(); i++)
-        {
-            if(text[i] != pattern[i % pattern.size()])
+    bool check(string &str, string &ans, int hcf) {
+        for(int i = 0; i < str.size(); i++) {
+            if(str[i] != ans[i % hcf])
                 return false;
         }
         return true;
     }
-    
-    string gcdOfStrings(string str1, string str2) {
+    string gcdOfStrings(string &str1, string &str2) {
         int hcf = __gcd(str1.size(), str2.size());
-        string pattern = str2.substr(0, hcf);
-        if(match(str1, pattern) && match(str2, pattern))
-            return pattern;
+        if(check(str1, str1, hcf) && check(str2, str1, hcf))
+            return str1.substr(0, hcf);
         return "";
     }
 };
