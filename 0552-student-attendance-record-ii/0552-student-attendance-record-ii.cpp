fc71ab55d358d9c0161221f11b80701cb0851271
Aditya Kumar Singh <56215647+adi2710@users.noreply.github.com>
2024-05-26 13:23:23 +0530

    Time: 4 ms (99.07%), Space: 16.4 MB (83.85%) - LeetHub

--- 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
@@ -1,30 +1,27 @@
-int memo[100001];
+int dp[100001][3][2];
 class Solution {
 public:
     const int MOD = 1e9 + 7;
     
-    int solve(int n, int late, int absent, vector<vector<vector<int>>> &dp) {
-        if(n < 0)
+    int solve(int n, int late, int absent) {
+        if(n == 0)
             return 1;
         if(dp[n][late][absent] != -1)
             return dp[n][late][absent];
-        int count = solve(n - 1, 0, absent, dp);
+        int count = solve(n - 1, 0, absent);
         if(absent == 0) {
-            count = (1ll * count + solve(n - 1, 0, absent + 1, dp)) % MOD;
+            count = (1ll * count + solve(n - 1, 0, absent + 1)) % MOD;
         }
         if(late <= 1) {
-            count = (1ll * count + solve(n - 1, late + 1, absent, dp)) % MOD;
+            count = (1ll * count + solve(n - 1, late + 1, absent)) % MOD;
         }
         return dp[n][late][absent] = count;
     }
 
     int checkRecord(int n) {
-        if(memo[0] == 0) {
-            memset(memo, -1, sizeof(memo));
+        if(dp[0][0][0] == 0) {
+            memset(dp, -1, sizeof(dp));
         }
-        if(memo[n] != -1)
-            return memo[n];
-        vector<vector<vector<int>>> dp(n, vector<vector<int>> (3, vector<int> (2, -1)));
-        return memo[n] = solve(n - 1, 0, 0, dp);
+        return solve(n, 0, 0);
     }
 };
